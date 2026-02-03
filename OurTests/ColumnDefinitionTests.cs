5ebb38bac3da3171c59d198aa54f821fa48598f0
NAIARA <ncalmaestra001@ikasle.ehu.eus>
2026-02-03 21:09:43 +0100

    #25 arreglar el fallo en merge

--- OurTests/ColumnDefinitionTests.cs
+++ OurTests/ColumnDefinitionTests.cs
@@ -3,35 +3,35 @@ namespace OurTests
 {
   public class ColumnDefinitionsTests
   {
-    [Theory]
-    [InlineData(ColumnDefinition.DataType.String, "Pepe")] 
-    [InlineData(ColumnDefinition.DataType.Int, "2")]
-    [InlineData(ColumnDefinition.DataType.Double, "2.0")]
-    [InlineData(ColumnDefinition.DataType.String, "")]
-    public void ColumnDefinition_Constructor_SetsPropertiesCorrectly(ColumnDefinition.DataType type, string name)
-    {
-      // Arrange & Act
-      var columnDefinition = new ColumnDefinition(type, name);
-      // Assert
-      Assert.Equal(type, columnDefinition.Type);
-      Assert.Equal(name, columnDefinition.Name);
-    }
+		[Theory]
+		[InlineData(ColumnDefinition.DataType.String, "Pepe")]
+		[InlineData(ColumnDefinition.DataType.Int, "2")]
+		[InlineData(ColumnDefinition.DataType.Double, "2.0")]
+		[InlineData(ColumnDefinition.DataType.String, "")]
+		public void ColumnDefinition_Constructor_SetsPropertiesCorrectly(ColumnDefinition.DataType type, string name)
+		{
+			// Arrange & Act
+			var columnDefinition = new ColumnDefinition(type, name);
+			// Assert
+			Assert.Equal(type, columnDefinition.Type);
+			Assert.Equal(name, columnDefinition.Name);
+		}
 
-    [Theory]
-    [InlineData(ColumnDefinition.DataType.String, "Pepe", "Pepe->String")]
-    [InlineData(ColumnDefinition.DataType.Int, "2", "2->Int")]
-    [InlineData(ColumnDefinition.DataType.Double, "2.0", "2.0->Double")]
-    [InlineData(ColumnDefinition.DataType.Double, "", "->Double")]
-    public void ColumnDefinition_AsText_WorkCorrectly(ColumnDefinition.DataType type, string name, string expectedReturn)
-    {
-      // Arrange
-      var columnDefinition = new ColumnDefinition(type, name);
-      //Act
-      var result = columnDefinition.AsText();
-      //Assert
-      Assert.Equal(expectedReturn, result);
-    }
-        [Fact]
+		[Theory]
+		[InlineData(ColumnDefinition.DataType.String, "Pepe", "Pepe->String")]
+		[InlineData(ColumnDefinition.DataType.Int, "2", "2->Int")]
+		[InlineData(ColumnDefinition.DataType.Double, "2.0", "2.0->Double")]
+		[InlineData(ColumnDefinition.DataType.Double, "", "->Double")]
+		public void ColumnDefinition_AsText_WorkCorrectly(ColumnDefinition.DataType type, string name, string expectedReturn)
+		{
+			// Arrange
+			var columnDefinition = new ColumnDefinition(type, name);
+			//Act
+			var result = columnDefinition.AsText();
+			//Assert
+			Assert.Equal(expectedReturn, result);
+		}
+		[Fact]
         public void ColumnDefinition_Constructor_TypeCorrecto() 
         {
             ColumnDefinition column = new ColumnDefinition(ColumnDefinition.DataType.Int, "Age");
@@ -44,5 +44,5 @@ namespace OurTests
             ColumnDefinition column = new ColumnDefinition(ColumnDefinition.DataType.String, "Name");
             Assert.Equal("Name", column.Name);
         }
-  }
+     }
 }
