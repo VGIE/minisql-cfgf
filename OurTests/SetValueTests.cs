1d26d863ba879ea5dac77323a201696f8300c2af
Gorka Delgado Fernández <gdelgado008@ikasle.ehu.eus>
2026-02-13 09:13:06 +0100

    #4 SetValue for SetValue.cs test and method implemented

---
+++ OurTests/SetValueTests.cs
@@ -0,0 +1,53 @@
+﻿using DbManager.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurTests
+{
+    public class SetValueTests
+    {
+        //TODO DEADLINE 1A : Create your own tests for Condition
+        [Fact]
+        public void SetValue_Constructor_ShouldSaveValuesCorrectly()
+        {
+            var s = new SetValue("Age", "21");
+            Assert.Equal("Age", s.ColumnName);
+            Assert.Equal("21", s.Value);
+        }
+
+        [Fact]
+        public void SetValue_Constructor_ShouldNotInitializeIfColumnIsNull()
+        {
+            var s = new SetValue(null, "21");
+            Assert.Null(s.ColumnName);
+            Assert.Null(s.Value);
+        }
+
+        [Fact]
+        public void SetValue_Constructor_ShouldNotInitializeIfColumnIsEmpty()
+        {
+            var s = new SetValue("", "21");
+            Assert.Null(s.ColumnName);
+            Assert.Null(s.Value);
+        }
+
+        [Fact]
+        public void SetValue_Constructor_ShouldNotInitializeIfColumnIsWhitespace()
+        {
+            var s = new SetValue(" ", "21");
+            Assert.Null(s.ColumnName);
+            Assert.Null(s.Value);
+        }
+
+        [Fact]
+        public void SetValue_Constructor_AllowsNullValue()
+        {
+            var s = new SetValue("Age", null);
+            Assert.Equal("Age", s.ColumnName);
+            Assert.Null(s.Value);
+        }
+    }
+}
