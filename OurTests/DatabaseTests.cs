6f8c7057fc64b4abbc11325c123593d90cfdcc7f
Izan <izan@lasadynamics.com>
2026-02-15 13:13:26 +0100

    #47 addtable tests implemented

--- OurTests/DatabaseTests.cs
+++ OurTests/DatabaseTests.cs
@@ -2,13 +2,38 @@ namespace OurTests
 {
     public class UnitTest1
     {
-        //TODO DEADLINE 1B : Create your own tests for Database
-        /*
-        [Fact]
-        public void Test1()
-        {
+    #region AddTable Tests
+    [Fact]
+    public void Database_AddTable_ShouldAddTableToDatabaseCorrectly()
+    {
+      //Assert
+      string tableName = "People";
+      List<DbManager.ColumnDefinition> columnDefinitions = new List<DbManager.ColumnDefinition>
+      {
+        (new DbManager.ColumnDefinition(DbManager.ColumnDefinition.DataType.Int, "Age")),
+        (new DbManager.ColumnDefinition(DbManager.ColumnDefinition.DataType.String, "Name")),
+        (new DbManager.ColumnDefinition(DbManager.ColumnDefinition.DataType.Double, "Height"))
+      };
 
-        }
-        */
+      List<string> valuesRow1 = new List<string> { "30", "Paco", "183.22" };
+      List<string> valuesRow2 = new List<string> { "22", "Miren", "165.08" };
+      List<string> valuesRow3 = new List<string> { "56", "Pedro", "188.57" };
+      List<string> valuesRow4 = new List<string> { "14", "Paco", "154.77" };
+
+      DbManager.Row row1 = new DbManager.Row(columnDefinitions, valuesRow1);
+      DbManager.Row row2 = new DbManager.Row(columnDefinitions, valuesRow2);
+      DbManager.Row row3 = new DbManager.Row(columnDefinitions, valuesRow3);
+      DbManager.Row row4 = new DbManager.Row(columnDefinitions, valuesRow4);
+
+      DbManager.Table table = new DbManager.Table(tableName, columnDefinitions);
+      var database = DbManager.Database.CreateTestDatabase();
+
+      //Act
+      database.AddTable(table);
+
+      //Assert
+      Assert.Equal(table, database.TableByName(tableName));
     }
+    #endregion
+  }
 }
