0a3f88201fc848c04c2f59e9befbd424ef6a0633
Izan <izan@lasadynamics.com>
2026-02-15 13:14:47 +0100

    #47 table by name tests implemented

--- OurTests/DatabaseTests.cs
+++ OurTests/DatabaseTests.cs
@@ -35,5 +35,42 @@ namespace OurTests
       Assert.Equal(table, database.TableByName(tableName));
     }
     #endregion
+
+    #region TableByName Tests
+    [Fact]
+    public void Database_TableByName_ShouldReturnTheCorrectTable()
+    {
+      //Assert
+      string tableName = "People";
+      List<DbManager.ColumnDefinition> columnDefinitions = new List<DbManager.ColumnDefinition>
+      {
+        (new DbManager.ColumnDefinition(DbManager.ColumnDefinition.DataType.Int, "Age")),
+        (new DbManager.ColumnDefinition(DbManager.ColumnDefinition.DataType.String, "Name")),
+        (new DbManager.ColumnDefinition(DbManager.ColumnDefinition.DataType.Double, "Height"))
+      };
+
+      List<string> valuesRow1 = new List<string> { "30", "Paco", "183.22" };
+      List<string> valuesRow2 = new List<string> { "22", "Miren", "165.08" };
+      List<string> valuesRow3 = new List<string> { "56", "Pedro", "188.57" };
+      List<string> valuesRow4 = new List<string> { "14", "Paco", "154.77" };
+
+      DbManager.Row row1 = new DbManager.Row(columnDefinitions, valuesRow1);
+      DbManager.Row row2 = new DbManager.Row(columnDefinitions, valuesRow2);
+      DbManager.Row row3 = new DbManager.Row(columnDefinitions, valuesRow3);
+      DbManager.Row row4 = new DbManager.Row(columnDefinitions, valuesRow4);
+
+      DbManager.Table table = new DbManager.Table(tableName, columnDefinitions);
+      var database = DbManager.Database.CreateTestDatabase();
+      database.AddTable(table);
+
+      //Act
+      var table1 = database.TableByName(tableName);
+      var table2 = database.TableByName("TestTable");
+
+      //Assert
+      Assert.Equal(DbManager.Table.CreateTestTable().ToString(), table2.ToString());
+      Assert.Equal(table.ToString(), table1.ToString());
+    }
+    #endregion
   }
 }
