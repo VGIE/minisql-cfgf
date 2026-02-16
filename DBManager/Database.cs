defaf608b3136168eb4a3114fff8dfdd1a763e78
Mario Romero Galdeano <mario.romerogaldeano@student.put.poznan.pl>
2026-02-16 21:20:08 +0100

    #48 create table function implemented

--- DBManager/Database.cs
+++ DBManager/Database.cs
@@ -52,9 +52,19 @@ namespace DbManager
             //return false and set LastErrorMessage with the appropriate error (Check Constants.cs)
             //Do the same if no column is provided
             //If everything goes ok, set LastErrorMessage with the appropriate success message (Check Constants.cs)
-            
-            return false;
-            
+            if (TableByName(tableName) != null)
+            {
+                LastErrorMessage = Constants.TableAlreadyExistsError;
+                return false;
+            }
+            else if (ColumnDefinition == null||ColumnDefinition.Count()==0)
+            {
+                LastErrorMessage = Constants.DatabaseCreatedWithoutColumnsError;
+                return false;
+            }
+            AddTable(new Table(tableName, ColumnDefinition));
+            LastErrorMessage = Constants.CreateTableSuccess;
+            return true;   
         }
 
         public bool DropTable(string tableName)
