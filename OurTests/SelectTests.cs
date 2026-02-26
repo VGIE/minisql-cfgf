8e44614bd07bb780d630fa03e0ce3b211954c8a6
Mario Romero Galdeano <mario.romerogaldeano@student.put.poznan.pl>
2026-02-26 11:13:28 +0100

    #12 select tests implemented

---
+++ OurTests/SelectTests.cs
@@ -0,0 +1,33 @@
+﻿using DbManager;
+
+namespace OurTests
+{
+    public class SelectTests
+    {
+        #region constructor tests
+        [Fact]
+        public void Select_Constructor_ShouldInitializeAttributes()
+        {
+            var table = "TestTable";
+            var columns = new List<String> { "Name", "Age" };
+            var condition = new DbManager.Condition("Name", "=", "Mario");
+            var select = new DbManager.Select(table, columns, condition);
+
+            Assert.Equal(table, select.Table);
+            Assert.Equal(columns, select.Columns);
+            Assert.Equal(condition, select.Where);
+        }
+        [Fact]
+        public void Select_Constructor_NoCondition_WhereShouldBeNull()
+        {
+            var table = "TestTable";
+            var columns = new List<String> { "Name", "Age" };
+            var select = new DbManager.Select(table, columns);
+
+            Assert.Equal(table, select.Table);
+            Assert.Equal(columns, select.Columns);
+            Assert.Null(select.Where);
+        }
+        #endregion
+    }
+}
