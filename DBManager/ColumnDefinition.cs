d5a57945fba27466a4c7ea68b5e3c2ee7daff1e4
NAIARA <ncalmaestra001@ikasle.ehu.eus>
2026-03-03 16:40:32 +0100

    #56 decode for columndefinition test and code implemented

--- DBManager/ColumnDefinition.cs
+++ DBManager/ColumnDefinition.cs
@@ -34,9 +34,10 @@ namespace DbManager
         private static string Decode(string value)
         {
             //TODO DEADLINE 1.C: Decode the delimiter in the value
-            
-            return null;
-            
+            if (value == null)
+            { return null;
+            }
+            return value.Replace(DelimiterEncoded, Delimiter);
         }
 
         public string AsText()
