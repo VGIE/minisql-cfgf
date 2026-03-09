22c26d9cbf39b6d734f6a9e0f36b6fd0f111ba14
Mario Romero Galdeano <mario.romerogaldeano@student.put.poznan.pl>
2026-03-09 12:30:30 +0100

    #66 drop table tests implemented

---
+++ OurTests/DropTableTests.cs
@@ -0,0 +1,17 @@
+﻿using DbManager;
+
+namespace OurTests
+{
+    public class DropTableTests
+    {
+        #region constructor tests
+        [Fact]
+        public void DropTable_Constructor_ShouldInitilizeAttributes()
+        {
+            var table = "TestTable";
+            var dropTable = new DbManager.DropTable(table);
+            Assert.Equal(table, dropTable.Table);
+        }
+        #endregion
+    }
+}
