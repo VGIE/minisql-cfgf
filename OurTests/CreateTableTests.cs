91e6924d66b12c4e3380bca01063bd86d156bb74
Izan <izan@lasadynamics.com>
2026-03-09 11:01:48 +0100

    #64 Create Table constructor tests implemented

---
+++ OurTests/CreateTableTests.cs
@@ -0,0 +1,28 @@
+﻿using DbManager;
+
+namespace OurTests
+{
+  public class CreateTableTests
+  {
+    #region Constructor Tests
+    [Fact]
+    public void CreateTable_Constructor_ShouldInitializeAttributesCorrectly()
+    {
+      //Arrange
+      var table = "TestTable";
+      var columns = new List<ColumnDefinition>()
+      {
+        (new ColumnDefinition(ColumnDefinition.DataType.String, "Name")),
+        (new ColumnDefinition(ColumnDefinition.DataType.Int, "Age"))
+      };
+
+      //Act
+      var createTable = new DbManager.CreateTable(table, columns);
+
+      //Assert
+      Assert.Equal(table, createTable.Table);
+      Assert.Equal(columns.Count, createTable.ColumnsParameters.Count);
+    }
+    #endregion
+  }
+}
