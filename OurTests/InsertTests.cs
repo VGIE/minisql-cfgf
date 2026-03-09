9788ec080b815af1f3d790989e7e51c4e6795bb2
NAIARA <ncalmaestra001@ikasle.ehu.eus>
2026-03-09 11:39:24 +0100

    #63 insert() for insert code implemented

---
+++ OurTests/InsertTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbManager;
+
+namespace OurTests
+{
+	public class InsertTests
+	{
+		
+	}
+}
