cf57db218a7c839d6ed5b9c558b0686b0a943b04
Izan <izan@lasadynamics.com>
2026-03-09 11:03:27 +0100

    #64 Create Table constructor implemented

--- DBManager/Parser/CreateTable.cs
+++ DBManager/Parser/CreateTable.cs
@@ -11,12 +11,13 @@ namespace DbManager
         public string Table { get; private set; }
         public List<ColumnDefinition> ColumnsParameters { get; private set; } = new List<ColumnDefinition>();
 
-        public CreateTable(string table, List<ColumnDefinition> columns)
-        {
-            //TODO DEADLINE 2: Initialize member variables
-            
-        }
-        public string Execute(Database database)
+    public CreateTable(string table, List<ColumnDefinition> columns)
+    {
+      //TODO DEADLINE 2: Initialize member variables
+      Table = table;
+      ColumnsParameters = columns;
+    }
+    public string Execute(Database database)
         {
             //TODO DEADLINE 3: Run the query and return the appropriate message
             //CreateTableSuccess or the last error in the database
