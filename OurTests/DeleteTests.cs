d6d33de7ba5bbaf0cff0591972ce3a297d27b71c
Izan Urrutikoetxea <izancorreop@gmail.com>
2026-03-16 21:43:15 +0100

    #71 delete constructor tests implemented

---
+++ OurTests/DeleteTests.cs
@@ -0,0 +1,24 @@
+﻿using DbManager;
+
+namespace OurTests
+{
+  public class DeleteTests
+  {
+    #region Constructor Tests
+    [Fact]
+    public void Delete_Constructor_ShouldInitializeAttributesCorrectly()
+    {
+      //Arrange
+      var table = "TestTable";
+      var condition = new DbManager.Condition("Name", "=", "Paco");
+
+      //Act
+      var delete = new DbManager.Parser.Delete(table, condition);
+
+      //Assert
+      Assert.Equal(table, delete.Table);
+      Assert.Equal(condition, delete.Where);
+    }
+    #endregion
+  }
+}
