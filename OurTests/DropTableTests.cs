aa27f9559548d34c24c720ce2d3e7ea9cdfa92b5
Mario Romero Galdeano <mario.romerogaldeano@student.put.poznan.pl>
2026-03-26 11:55:55 +0100

    #72 execute drop table tests implemented

--- OurTests/DropTableTests.cs
+++ OurTests/DropTableTests.cs
@@ -13,5 +13,29 @@ namespace OurTests
             Assert.Equal(table, dropTable.Table);
         }
         #endregion
+
+        #region execute tests
+        [Fact]
+        public void DropTable_Execcute_ShouldWorkCorrectly()
+        {
+            var table = "TestTable";
+            var dropTable = new DbManager.DropTable(table);
+            var database = Database.CreateTestDatabase();
+            var result = dropTable.Execute(database);
+
+            Assert.Equal(Constants.DropTableSuccess, result);
+            Assert.Null(database.TableByName(table));//la tabla no debe exisitir en la base de datos si execute lo ha borrado
+        }
+        [Fact]
+        public void DropTable_Execute_TableDoesntExist_ShouldReturnError()
+        {
+            var table = "NoExiste";
+            var dropTable = new DbManager.DropTable(table);
+            var database = Database.CreateTestDatabase();
+            var result = dropTable.Execute(database);
+
+            Assert.Equal(database.LastErrorMessage, result);
+        }
+        #endregion
     }
 }
