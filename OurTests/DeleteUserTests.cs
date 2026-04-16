8bfc38873799836cbef0b5a1b704a8b1ec31d36e
Mario Romero Galdeano <mario.romerogaldeano@student.put.poznan.pl>
2026-04-16 09:40:56 +0200

    #79 delete user constructor tests implemented

---
+++ OurTests/DeleteUserTests.cs
@@ -0,0 +1,18 @@
+﻿using DbManager;
+
+namespace OurTests
+{
+    public class DeleteUserTests
+    {
+        #region Constructor Tests
+        [Fact]
+        public void DeleteUser_Constructor_ShouldInitializeAttributesCorrectly()
+        {
+            var username = "Juancillo";
+            var deleteUser = new DeleteUser(username);
+
+            Assert.Equal(username, deleteUser.Username);
+        }
+        #endregion
+    }
+}
