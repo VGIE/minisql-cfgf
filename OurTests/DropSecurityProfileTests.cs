2a05108e5f84331b85b6cb78af38542a48d92a44
Mario Romero Galdeano <mario.romerogaldeano@student.put.poznan.pl>
2026-04-16 09:48:56 +0200

    #80 drop security profile const test

---
+++ OurTests/DropSecurityProfileTests.cs
@@ -0,0 +1,17 @@
+﻿using DbManager;
+namespace OurTests
+{
+    public class DropSecurityProfileTests
+    {
+        #region Constructor Tests
+        [Fact]
+        public void DropSecurityProfile_Constructor_ShouldInitializeAttributesCorrectly()
+        {
+            var profileName = "Jonathan";
+            var dropSecurityProfile = new DropSecurityProfile(profileName);
+
+            Assert.Equal(profileName, dropSecurityProfile.ProfileName);
+        }
+        #endregion
+    }
+}
