35c2e534ab95b845eefdec58e65cbf9508655ee5
Izan Urrutikoetxea <izancorreop@gmail.com>
2026-04-29 10:13:36 +0200

    #95 revoke class constructor tests implemented

---
+++ OurTests/RevokeTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbManager;
+
+namespace OurTests
+{
+  public class RevokeTests
+  {
+    #region Constructor Tests
+    [Fact]
+    public void Revoke_Constructor_ShouldInitializeAttributesCorrectly()
+    {
+      //Arrange
+      var privilegeName = "pepito";
+      var tableName = "TestTable";
+      var profileName = "juan";
+
+      //Act
+      var revoke = new Revoke(privilegeName, tableName, profileName);
+
+      //Assert
+      Assert.Equal(privilegeName, revoke.PrivilegeName);
+      Assert.Equal(tableName, revoke.TableName);
+      Assert.Equal(profileName, revoke.ProfileName);
+
+    }
+    #endregion
+  }
+}
