90663cd114676e369ce9d53873caffc73103133e
Mario Romero Galdeano <mario.romerogaldeano@student.put.poznan.pl>
2026-05-01 15:05:28 +0200

    #109 revoke privilege execute method and tests implemented

--- OurTests/RevokeTests.cs
+++ OurTests/RevokeTests.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using DbManager;
+using DbManager.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DbManager;
 
 namespace OurTests
 {
@@ -27,6 +28,62 @@ namespace OurTests
       Assert.Equal(profileName, revoke.ProfileName);
 
     }
-    #endregion
-  }
+        #endregion
+
+    #region execute test
+    [Fact]
+    public void Revoke_Execute_ShouldWork_WhenUserIsAdmin()
+    {
+        var database = Database.CreateTestDatabase();
+        var profile = new Profile { Name = "Profile" };
+        profile.GrantPrivilege("Table", Privilege.Select);
+        database.SecurityManager.Profiles.Add(profile);
+        var revoke = new Revoke("SELECT", "Table", "Profile");
+        var result = revoke.Execute(database);
+
+        Assert.Equal(Constants.RevokePrivilegeSuccess, result);
+        Assert.False(profile.IsGrantedPrivilege("Table", Privilege.Select));
+    }
+
+    [Fact]
+    public void Revoke_Execute_ShouldReturnError_IfProfileDoesNotExist()
+    {
+        var database = Database.CreateTestDatabase();
+        var profile = new Profile { Name = "Profile" };
+        database.SecurityManager.Profiles.Add(profile);
+        database.SecurityManager.Profiles.Add(profile);
+        var revoke = new Revoke("SELECT", "Table", "Profile1");
+        var result = revoke.Execute(database);
+
+        Assert.Equal(Constants.SecurityProfileDoesNotExistError, result);
+    }
+
+    [Fact]
+    public void Revoke_Execute_ShouldReturnError_WhenUserIsNotAdmin()
+    {
+        var database = new Database("Mario", "1234");
+        database.SecurityManager.RemoveProfile(Profile.AdminProfileName);
+        var profile = new Profile { Name = "Profile" };
+        profile.Users.Add(new User { Username = "Mario" });
+        database.SecurityManager.Profiles.Add(profile);
+        var revoke = new Revoke("SELECT", "Table", "Profile");
+        var result = revoke.Execute(database);
+
+        Assert.Equal(Constants.UsersProfileIsNotGrantedRequiredPrivilege, result);
+    }
+
+    [Fact]
+    public void Revoke_Execute_ShouldReturnError_IfPrivilegeDoesNotExist()
+    {
+        var database = Database.CreateTestDatabase();
+        var profile = new Profile { Name = "Profile" };
+        database.SecurityManager.Profiles.Add(profile);
+        var revoke = new Revoke("kajdkjdflk", "Table", "Profile");
+        var result = revoke.Execute(database);
+
+        Assert.Equal(Constants.PrivilegeDoesNotExistError, result);
+    }
+
+        #endregion
+    }
 }
